744d2083e9893b0425b35f8dffe4aaddff0935c4
MaxPirojkov <36989427+MaxPirojkov@users.noreply.github.com>
2021-07-22 13:44:54 +0300

    Feature/4 list homework (#2)
    * step 2 homework
    
    * add ratingbar and toast
    
    * add some fix
    
    * fix margins
    
    * add fix after review
    
    * update onItemClick
    
    Co-authored-by: Maxim <m.pirozhkov@city-mobil.ru>

--- app/src/main/java/com/mtsteta/android/pirozhkovteta/MainActivity.kt
+++ app/src/main/java/com/mtsteta/android/pirozhkovteta/MainActivity.kt
@@ -2,10 +2,23 @@ package com.mtsteta.android.pirozhkovteta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        setContentView(R.layout.activity_main)
+        val recycler = findViewById<RecyclerView>(R.id.rcView)
+        val movies = MoviesDataSourceImpl().getMovies()
+        val adapter = PreviewMovieAdapter(this) { onItemClick(it) }
+        recycler.adapter = adapter
+        recycler.layoutManager = GridLayoutManager(this@MainActivity, 2)
+        adapter.updates(movies)
+    }
+
+    private fun onItemClick(title: String) {
+        Toast.makeText(this, title, Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/mtsteta/android/pirozhkovteta/PreviewMovie.kt
@@ -0,0 +1,8 @@
+package com.mtsteta.android.pirozhkovteta
+
+data class PreviewMovie(
+    val title: String,
+    val description: String,
+    val rateScore: Int,
+    val ageRestriction: String,
+    val imageUrl: String)
