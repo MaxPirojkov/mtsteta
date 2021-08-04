93d55143fdb875ef315e15d5dcd4bd21bd049152
MaxPirojkov <36989427+MaxPirojkov@users.noreply.github.com>
2021-08-04 19:59:13 +0300

    Revert "Feature/5 UI components homework (#3)"
    This reverts commit e4cbe063ce374ca7c02c75143bbed0fc26324b2f.

--- app/src/main/java/com/mtsteta/android/pirozhkovteta/GlideModule.kt
+++
@@ -1,6 +0,0 @@
-package com.mtsteta.android.pirozhkovteta
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class GlideModule : AppGlideModule(){}
--- app/src/main/java/com/mtsteta/android/pirozhkovteta/MoviesDataSource.kt
+++
@@ -1,5 +0,0 @@
-package com.mtsteta.android.pirozhkovteta
-
-interface MoviesDataSource {
-    fun getMovies(): List<PreviewMovie>
-}
